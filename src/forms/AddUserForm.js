b429ade0a7757bb29d2afa018220e0b61661530a
tabuguia <tabuguiafrank@gmail.com>
2023-04-18 19:48:39 +0200

    ajout du handleInputChange

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
 
 function AddUserForm() {
+    const initialFormState = { id: null, name: "", username: "" }
+    const [user, setUsers] = useState(initialFormState);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUsers({ ...user, [name]: value });
+    }
+
     return (
         <form >
             <label htmlFor="">Name</label>
