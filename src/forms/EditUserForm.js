e4f0a4c3c29d5babf2ecf41d4d2a8f8ed23d62ec
tabuguia <tabuguiafrank@gmail.com>
2023-04-19 11:21:12 +0200

    ajustement erreurs 2

---
+++ src/forms/EditUserForm.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function EditUserForm(props) {
+    const [user, setUser] = useState(props.currentUser);
+
+    useEffect(() => {
+        setUser(props.currentUser)
+    }, [props])
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUser({ ...user, [name]: value });
+    }
+
+    return (
+        <form onSubmit={event => {
+            event.preventDefault();
+            props.updateUser(user.id, user);
+        }} >
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" value={user.name} onChange={handleInputChange} />
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" value={user.username} onChange={handleInputChange} />
+            <button>Modifier</button>
+            <button onClick={() => props.setEditing(false)} className=" button muted-button" >Annuler</button>
+        </form>
+    );
+}
+
+export default EditUserForm;
